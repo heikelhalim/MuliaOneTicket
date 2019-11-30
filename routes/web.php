1679a61ec8b75309e6a510935510621a41d45c52
heikelhalim <48664086+heikelhalim@users.noreply.github.com>
2019-11-30 16:09:45 +0800

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', function () {
+	if(Auth::guest()){
+    	return view('welcome');
+	}else{
+		return redirect('/home');
+	}
+});
+
+
+
+Auth::routes();
+
+
+
+//Route::get('/home', 'HomeController@index')->name('home');
+
+//Auth::routes();
+
+//Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/home', function(){
+	return redirect(action('\Kordy\Ticketit\Controllers\TicketsController@index'));
+});
